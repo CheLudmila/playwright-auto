4ee04c3981d60df2c2530fdf2e9a37dc1655ac09
Liudmyla Cheredniuk <Sapphire777.astro@gmail.com>
2026-05-29 00:16:51 +0300

    Initialize Playwright project

---
+++ playwright.config.js
@@ -0,0 +1,31 @@
+import { defineConfig } from '@playwright/test';
+
+export default defineConfig({
+  testDir: './tests',
+
+  timeout: 30000,
+
+  fullyParallel: true,
+
+  reporter: [
+    ['html'],
+  ],
+
+  use: {
+    baseURL: 'https://qauto.forstudy.space',
+
+    headless: false,
+
+    viewport: {
+      width: 1280,
+      height: 720,
+    },
+
+    screenshot: 'only-on-failure',
+
+    video: 'retain-on-failure',
+
+    trace: 'on-first-retry',
+  },
+});
+
